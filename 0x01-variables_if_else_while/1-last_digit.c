77cfec1d3429b33cc27ff6e6b09b83b03ab0970c
blessing-law <blessing.lawrence77@gmail.com>
2022-09-09 07:14:00 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -31,7 +31,7 @@ int main(void)
 				}
 				else if (last < 6 && n != 0)
 				{
-					printf("Last digit of %d is %d and is less than 6 and not 0", n, last);
+					printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 				}
 					return (0);
 }
