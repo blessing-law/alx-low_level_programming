bd6d644a60f954a8edaeae2fd981e08875dc5a72
blessing-law <blessing.lawrence77@gmail.com>
2022-09-09 12:29:57 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alpha;
+
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			putchar(alpha);
+		}
+		putchar('\n');
+		return (0);
+}
