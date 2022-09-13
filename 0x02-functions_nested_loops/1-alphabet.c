16eb7efe888889eccfec419860361b7d18d87f14
blessing-law <blessing.lawrence77@gmail.com>
2022-09-13 11:20:05 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Prints alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet()
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+			_putchar('\n');
+		}
+	}
+}
