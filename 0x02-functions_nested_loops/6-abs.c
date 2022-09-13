d2600fe551c2f50ed5903c95dc2e92bd9baacb86
blessing-law <blessing.lawrence77@gmail.com>
2022-09-13 15:05:20 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - print absolute value
+ *
+ * Return: result of function
+ * @int: interger to be calculated
+ */
+
+int abs(int);
+
+int _abs(int)
+{
+	return (abs(int));
+}
