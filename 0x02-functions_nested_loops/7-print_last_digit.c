1e8bddaa9bca4042f2d1f5a18061f5fc511b239c
blessing-law <blessing.lawrence77@gmail.com>
2022-09-13 15:16:44 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - pritn the last digit of number
+ *
+ * @i: parameter to be added
+ * Return:Value of i
+ */
+int print_last_digit(int i)
+{
+	return (i % 10);
+}
