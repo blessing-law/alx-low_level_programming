d8d51ce3499ae437a1d6fd9d0d97c972262bcb4a
blessing-law <blessing.lawrence77@gmail.com>
2022-09-13 14:40:56 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int _isalpha(int c) - checks for alphabetic character
+ *@c: character to be checked
+ *
+ * Return: 1 if letter, 0 if otherwise
+ */
+int isalpha(int c);
+
+int _isalpha(int c)
+{
+	if (isalpha(c)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
