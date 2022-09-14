502f1d7f2827b2bd573f26ff504e864ecc0c9c61
blessing-law <blessing.lawrence77@gmail.com>
2022-09-14 06:11:24 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,18 +7,11 @@
  */
 void jack_bauer(void)
 {
-	int i = '00';
 	int j;
 
-	while (i < 24)
-	{
-		_putchar(i);
-		_putchar(':');
 		for (j = '0'; j < 60; j++)
 		{
 			_putchar(j);
 		}
-		i++;
-	}
 	_putchar('\n');
 }
