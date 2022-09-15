7307c13c64656cfb20ca955660a66d9a931c17c1
blessing-law <blessing.lawrence77@gmail.com>
2022-09-15 06:51:02 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_numbers - print numbers
  *
- * Return: 0
+ * Return: void
  */
 
 void print_numbers(void)
@@ -15,5 +15,4 @@ void print_numbers(void)
 		_putchar(i);
 	}
 	_putchar('\n');
-	return (0);
 }
