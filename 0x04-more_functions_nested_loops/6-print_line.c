1c90e20b433b495cc852aa77fcc3b76be3e740ce
blessing-law <blessing.lawrence77@gmail.com>
2022-09-15 10:47:27 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  print_line - write a straight line in the terminal
+ *  @n: The number of lines
+ *
+ *  Return: 0
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		while (x--)
+		{
+			_putchar('_');
+		}
+		_putchar('\n')
+	}
+	else
+	{
+		_putchar('\n')
+	}
+}
