dbf0a612b1f97cfd36ca1b70bb51ea0e57da3ac9
blessing-law <blessing.lawrence77@gmail.com>
2022-09-19 10:43:32 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - find the length of a string
+ * @*s: input string
+ *
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = strlen(*s);
+	return (i);
+}
