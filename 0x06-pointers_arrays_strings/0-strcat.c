8437c86666e0d6cea058b311e3fa7948865a08a0
blessing-law <blessing.lawrence77@gmail.com>
2022-09-21 08:36:11 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcat - concatinate two strings
+ *
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+
+	return (0);
+}
