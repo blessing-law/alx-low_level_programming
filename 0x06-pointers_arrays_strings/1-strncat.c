00f3739225d5d35ba13338ffcd1da4709b1dac15
blessing-law <blessing.lawrence77@gmail.com>
2022-09-22 04:42:06 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include "main.h"
+#include <string.h>
 
 /**
  * _strncat - concateneate two strings
- * @
+ * @dest: first string
+ * @src: second string
+ * @n: number of characters from second string
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return (0);
+}
