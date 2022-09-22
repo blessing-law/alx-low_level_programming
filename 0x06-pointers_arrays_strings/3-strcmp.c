aacb5751edf4db8d89eaaebab88ac64f279a66e0
blessing-law <blessing.lawrence77@gmail.com>
2022-09-22 06:45:03 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ *
+ * Return: -15 if s1<s2, 0 if s1=s2, 15 if s1>s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int str;
+
+	str = strcmp(s1, s2);
+
+	if (str < 0)
+	{
+		return (-15);
+	}
+	else if (str > 0)
+	{
+		return (15);
+	}
+	else 
+	{
+		return (0);
+	}
+}
