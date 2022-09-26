fbe2614abf996acf8c79540c1738d3f14ce661e4
blessing-law <blessing.lawrence77@gmail.com>
2022-09-26 08:05:00 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strchr - locate character in string
+ * @s: string to be searched
+ * @c: character to be searched for
+ *
+ * Return: character c or null
+ */
+
+char *_strchr(char *s, char c)
+{
+	strchr(s, c);
+	return (0);
+}
