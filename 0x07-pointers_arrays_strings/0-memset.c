cac4111b4ee78f419bb4e893985b32fcdc469cdc
blessing-law <blessing.lawrence77@gmail.com>
2022-09-26 07:46:46 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * *_memset(char *s, char b, unsigned int n) - fill memeory with constant byte
+ *
+ * @s: pointer to memory to eb filled
+ * @b: value to be set
+ * @n: number of bytes
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(s, b, n);
+	return (s);
+}
