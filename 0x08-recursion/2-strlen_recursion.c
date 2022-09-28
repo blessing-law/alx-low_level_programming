cbb478dc4f5174b1c31ddb5ca54b3e33613465d5
blessing-law <blessing.lawrence77@gmail.com>
2022-09-28 08:05:19 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - find length of string
+ * @s: input string
+ *
+ * Return: string length
+ */
+
+int _strlen_recursion(char *s)
+{
+	int l;
+
+	l = strlen(s);
+	return (l);
+}
