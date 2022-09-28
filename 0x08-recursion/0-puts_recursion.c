bc8170782ee978c918bf9f408e0a9283c758f3e4
blessing-law <blessing.lawrence77@gmail.com>
2022-09-28 06:45:00 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - prints string followed by new line
+ * @s: string to be printed
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
+
