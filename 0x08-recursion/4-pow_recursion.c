b32087c444008f2b4c08d6eeb0179045ae6cdf3c
blessing-law <blessing.lawrence77@gmail.com>
2022-09-28 08:28:52 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - calculate x to the power of y
+ * @x: root number
+ * @y: expoonent
+ *
+ * Return: result or -1 if error
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int p;
+
+	p = pow(x, y);
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (p);
+}
