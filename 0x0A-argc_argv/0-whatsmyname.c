94706ca0952f2456462012c93f82b11da1265d43
blessing-law <blessing.lawrence77@gmail.com>
2022-09-30 08:15:51 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _print_name: print name of function
+ *
+ * Return: void
+ */
+
+void _print_name(void)
+{
+	_putchar(_print_name);
+	_putchar('\n');
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+int _putchar(char c);
+void _print_name(void);
+
+#endif
