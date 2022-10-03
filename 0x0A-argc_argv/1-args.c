c00b49e93e5b91af325916f7fdb1418bc1fedcb8
blessing-law <blessing.lawrence77@gmail.com>
2022-10-03 06:03:04 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_argument - print all arguments
+ * @argc: arguments count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int print_argument(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%d\n", argc[i];
+	}
+	return (0);
+}
