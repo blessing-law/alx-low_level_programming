874f6e927a097c03cca5899c77d0e7f560e0a399
blessing-law <blessing.lawrence77@gmail.com>
2022-10-03 02:37:32 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,14 +3,14 @@
 #include "main.h"
 
 /**
- * _print_name - print name of function
+ * main - print name of function
  * @argc: argument count
  * @argv: argument vector
  *
  * Return: void
  */
 
-void _print_name(int argc, char *argv)
+void main(int argc, char *argv)
 {
 	int i;
 
