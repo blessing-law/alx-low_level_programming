ec1762847647c9847d9a386f18f159d162007cd7
blessing-law <blessing.lawrence77@gmail.com>
2022-10-04 02:05:45 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,14 +4,14 @@
 #include <stdlib.h>
 
 /**
- * main - print number of arguments
+ * print_argument - print number of arguments
  * @argc: number of arguments
  * @argv: argument vector
  *
  * Return: 0
  */
 
-int main(int argc, __attribute__((unused)) char *argv[])
+int print_argument(int argc, __attribute__((unused)) char *argv[])
 {
 	_putchar(argc);
 	_putchar('\n');
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,5 +3,6 @@
 
 int _putchar(char c);
 void _print_name(void);
+int print_argument(int argc, __attribute__((unused)) char *argv[]);
 
 #endif
