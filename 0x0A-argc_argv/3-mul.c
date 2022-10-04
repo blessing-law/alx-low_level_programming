f13a4cc08cdc9e51532001f809d24f991c76a3a4
blessing-law <blessing.lawrence77@gmail.com>
2022-10-04 05:55:18 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,18 +13,10 @@
 
 int main(__attribute__((unused)) int argc, char *argv[])
 {
-	int mul1;
-	int mul2;
-	int ret;
-
-	mul1 = atoi(argv[1]);
-	mul2 = atoi(argv[2]);
-	ret = mul1 * mul2;
-
-	if (mul1 && mul2)
+	if (argv[1] && argv[2])
 	{
-		printf("%d\n", ret);
-		return (0);
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+			return (0);
 	}
 	else
 	{
