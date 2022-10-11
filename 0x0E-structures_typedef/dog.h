d70a330cecba3116f340d75545a4ec62a3c37d6b
blessing-law <blessing.lawrence77@gmail.com>
2022-10-11 05:13:57 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - struct with name, age and owner
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: struct that gives information about a specific dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
