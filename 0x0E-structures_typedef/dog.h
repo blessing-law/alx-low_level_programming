dade55b63654f27c8ef9210663eeda52d1c1b817
blessing-law <blessing.lawrence77@gmail.com>
2022-10-11 12:32:41 -0700

    3-dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,19 +11,13 @@
  * Description: struct that gives information about a specific dog
  */
 
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
