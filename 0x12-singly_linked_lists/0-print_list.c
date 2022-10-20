ea05f466364aa35654f61600bbc304d43f1fdfbf
blessing-law <blessing.lawrence77@gmail.com>
2022-10-20 06:52:32 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,15 +13,18 @@ size_t print_list(const list_t *h)
 {
 	size_t i = 0;
 
-	if (h->str == NULL)
-	{
-		printf("[0] (nil)\n");
-	}
 	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
-		i++;
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
 		h = h->next;
+		i++;
 	}
 	return (i);
 }
