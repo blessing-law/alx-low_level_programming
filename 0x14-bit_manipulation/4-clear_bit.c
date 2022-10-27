49c4aaf027614c3efc7a3fa3dc26c3a6d813778f
blessing-law <blessing.lawrence77@gmail.com>
2022-10-27 12:13:54 -0700

    4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * set value at index to 0
+ * @n: input value
+ * @index: index to be changed
+ *
+ * Return: 1 if success or -1 if fail
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i;
+
+	if (index > 63)
+	{
+		return (-1);
+	}
+
+	i = 1 << index;
+
+	if (*n & i)
+	{
+		*n ^= i;
+	}
+
+	return (1);
+}
