59c0f0388434d18b382fd9404f3559c152895c90
blessing-law <blessing.lawrence77@gmail.com>
2022-10-27 12:27:02 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,11 +12,11 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int i;
 
-	for (i = 0; n || m; n >>= 1. m >>= 1)
+	for (i = 0; n || m; n >>= 1, m >>= 1)
 	{
 		if ((n & 1) != (m & 1))
 		{
-			i++
+			i++;
 		}
 	}
 	return (i);
