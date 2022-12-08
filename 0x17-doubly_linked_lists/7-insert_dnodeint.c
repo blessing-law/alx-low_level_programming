583f9c5d2d7305560306e6331e8cff3b5b1eeb2d
blessing-law <blessing.lawrence77@gmail.com>
2022-12-08 11:10:57 -0800

    extra

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - insert node at position
+ * @h: pointer to fisrt node
+ * @idx: position where node is inserted
+ * @n: interger to be inserted
+ *
+ * Return: adress of new node or NULL
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+
+}
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - delete node at index
+ * @head: pointer to first node
+ * @index: postion of node
+ *
+ * Return: 1 if successful and -1 if failed
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+{
+
+}
